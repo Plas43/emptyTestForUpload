6f4230334415027c84afeffbc078086f811c4ee6
Plas43 <71021956+Plas43@users.noreply.github.com>
2022-06-12 15:49:11 -0400

    Update ForUpload.java

--- src/ForUpload.java
+++ src/ForUpload.java
@@ -2,5 +2,6 @@ public class ForUpload {
     public static void main(String[] args) {
         System.out.println("uploaded!");
         System.out.println("committed!");
+        System.out.println("new branch");
     }
 }
